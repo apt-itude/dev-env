76cd410f484cf0217ac6aa8e5ed84e4620d5e9bb
Alex Thompson <aptbosox@gmail.com>
2018-07-31 00:33:01 -0400

    Fixed indentation in install_ansible script

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,7 +3,7 @@
 PLATFORM=`uname -s`
 
 if [[ "$PLATFORM" == 'Darwin' ]]; then
-	sudo chown -R $USER /usr/local/
+    sudo chown -R $USER /usr/local/
     if [ ! -x "$(command -v brew)" ]; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
