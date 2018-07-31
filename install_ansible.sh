56716820fc2961fba47ef8d4c11f3e2e666d8f7a
Alex Thompson <aptbosox@gmail.com>
2018-07-30 23:02:45 -0400

    Changed install_ansible script to install homebrew and then ansible via homebrew

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,5 +3,10 @@
 PLATFORM=`uname -s`
 
 if [[ "$PLATFORM" == 'Darwin' ]]; then
-	sudo pip install ansible
+	sudo chown -R $USER /usr/local/
+    if [ ! -x "$(command -v brew)" ]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    brew update
+    brew install ansible
 fi
