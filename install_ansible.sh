1e2f7a9979e547be0675c140659945179f66ae00
Alex Thompson <aptbosox@gmail.com>
2018-09-05 10:06:01 -0400

    Fixed permission issue in init script on High Sierra

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+set -e
 
 PLATFORM=`uname -s`
 
 if [[ "$PLATFORM" == 'Darwin' ]]; then
-    sudo chown -R $USER /usr/local/
     if [ ! -x "$(command -v brew)" ]; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
+    sudo chown -R $(whoami) $(brew --prefix)/*
     brew update
     brew install ansible
 fi
